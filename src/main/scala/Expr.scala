295b645b64bef72ffc0fc8e089d57f57398442a9
Adianto Wibisono <awibisono@ripe.net>
2019-03-10 09:29:41 +0100

    Add collatz example, I am not sure I know what I am doing

--- src/main/scala/Expr.scala
+++ src/main/scala/Expr.scala
@@ -2,6 +2,7 @@ object Expr extends App {
 
   import matryoshka._, implicits._, data._
   import scalaz._
+  import std.option._, std.list._
 
   sealed abstract class Expr[A]
   final case class Num[A](value: Long) extends Expr[A]
@@ -17,6 +18,7 @@ object Expr extends App {
     }
   }
 
+
   val eval: Algebra[Expr, Long] = { // i.e. Expr[Long] => Long
     case Num(x)    => x
     case Mul(x, y) =>  x * y
@@ -27,6 +29,12 @@ object Expr extends App {
     case Num(x) => s"$x"
     case Mul(x, y) => s"($x * $y)"
     case Plus(x, y) => s"$x + $y"
+  }
+
+  val showM: AlgebraM[List, Expr, String] = {
+    case Num(x) => List(s"$x")
+    case Mul(x, y) => List(s"($x * $y)")
+    case Plus(x, y) => List(s"$x + $y")
   }
 
   val pluses: Coalgebra[Expr, Long] = x => {
@@ -42,9 +50,20 @@ object Expr extends App {
     if(x>1)  Mul(-\/(Fix(Num(x))), \/-(x-1)) else Num(1)
   }
 
+  val collatz: GCoalgebra[EitherFix, Expr, Long] = x => x match {
+    case 0  => Num(0)
+    case 1  => Num(1)
+    case n  => if (n%2 == 0) 
+                  Mul(-\/(Fix(Num(n))), \/-(n / 2))
+                else
+                  Mul(-\/(Fix(Num(n))), \/-(3*n+1))
+  }
+
   val expFact = 12L.apo[Fix[Expr]](fact)
+  val colSiz = 6L.apo[Fix[Expr]](collatz)
 
   println(expFact.cata(show))
   println(expFact.cata(eval))
+  println(colSiz.cata(show))
 
 }
--- src/main/scala/Sudoku.scala
+++ src/main/scala/Sudoku.scala
@@ -1,42 +1,75 @@
-import matryoshka._, implicits._, data._
+import Expr.Expr
+import matryoshka._
+import implicits._
+import data._
 import scalaz._
 
-object Sudoku extends App  {
+
+object Sudoku extends App {
   val N = 3
+
   sealed trait Cell
-  case class Fill(value : Int) extends Cell
-  case class Slot(possible : List[Int]) extends Cell
 
-  val empty = Slot( (1 to N).toList)
+  case class Fill(value: Int) extends Cell
 
-  type Cells = Array[Cell]
+  case class Slot(possible: List[Int]) extends Cell
 
+  val initSlot: Cell = Slot((1 to N).toList)
+
+  type Row = Array[Cell]
+  type Rows = Array[Row]
 
   sealed abstract class Board[A]
-  case class Solved[A]( rows : Cells) extends Board[A]
-  case class Choice[A](child : List[A]) extends Board[A]
 
- implicit val boardFunctor: Functor[Board] = new scalaz.Functor[Board] {
+  case class Single[A](cells: Rows) extends Board[A]
+
+  case class Choice[A](child: List[A]) extends Board[A]
+
+  implicit val boardFunctor: Functor[Board] = new scalaz.Functor[Board] {
     override def map[A, B](fa: Board[A])(f: (A) => B): Board[B] = fa match {
-      case Solved(rows) => Solved[B](rows)
+      case Single(rows) => Single[B](rows)
       case Choice(childs) => Choice(childs.map(f))
     }
   }
 
-val show: Algebra[Board, String] = {
-  case Solved(cells) => cells.sliding(N,N).map(_.mkString(" ")).mkString("\n")
-  case Choice(childs) => childs.mkString("\n--|--\n")
-}
+  val show: Algebra[Board, String] = {
+    case Single(rows) => rows.map(_.mkString(" ")).mkString("\n")
+    case Choice(childs) => childs.mkString("\n--|--\n")
+  }
 
-val gen: Coalgebra[Board, Cells] = rows => {
-   Solved(rows)
-}
+  def clearCell(v: Int) : Cell => Cell = {
+    case Slot(pos) => Slot(pos.filterNot(_ == v))
+    case Fill(v) => Fill(v)
+  }
 
-val init: Array[Cell] = (1 to 9).toArray.map(_=>empty)
+  def clearRow(v: Int) : Row => Row = { r => r map clearCell(v) }
 
 
-println(hylo(init)(show, gen))
+  def clearCol(col: Int, v: Int, rows: Rows) = {
+    val res = rows.clone()
+    (0 until N).foreach { row =>
+      res(row)(col) = clearCell(v)(res(row)(col))
+    }
+    res
+  }
 
+  def expand(rows: Rows) : List[Rows] = List(rows)
+
+  type EitherFix[T] = \/[Fix[Board], T]
+
+  val gen: Coalgebra[ Board, Rows] = rows => {
+    val expanded = expand(rows)
+    if(expanded.size == 1)
+      Single(expanded.head)
+    else
+      Choice(expanded)
+  }
+
+  val init: Rows = (1 to N * N).toArray.sliding(N, N).map(_.map(_ => initSlot)).toArray
+
+  val single = Single(init)
+  init(1) = clearRow(2)(init(1))
+  println(hylo(init)(show, gen))
 
 
 }
